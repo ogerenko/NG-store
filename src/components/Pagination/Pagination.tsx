c156216b833898685fdf987ba53ca69d5945e5ad
Ruslan Ogerenko <ruslanogerenko@gmail.com>
2024-08-09 23:20:08 +0300

    clone store gadget react

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -54,46 +54,44 @@ export const Pagination: React.FC<Props> = ({ products }) => {
     scrollOnTop(200);
   };
 
-  return (
-    amountOfPages().length > 1 && (
-      <div className="pagination">
-        <button
-          className={classNames('pagination__button pagination__button--prev', {
-            'pagination__button--prev--disabled':
-              +pageParam === amountOfPages()[0],
-          })}
-          disabled={+pageParam === amountOfPages()[0]}
-          onClick={handlePrevPageChange}
-        />
+  return amountOfPages().length > 1 ? (
+    <div className="pagination">
+      <button
+        className={classNames('pagination__button pagination__button--prev', {
+          'pagination__button--prev--disabled':
+            +pageParam === amountOfPages()[0],
+        })}
+        disabled={+pageParam === amountOfPages()[0]}
+        onClick={handlePrevPageChange}
+      />
 
-        {adaptivePaginationPages(amountOfPages(), +pageParam).map(
-          (page, index) =>
-            page !== '...' ? (
-              <button
-                className={classNames('pagination__button', {
-                  pagination__active: +pageParam === page,
-                })}
-                key={page}
-                onClick={() => handlePageChange(+page)}
-              >
-                {page}
-              </button>
-            ) : (
-              <div className="pagination__darks" key={`${page}${index}`}>
-                {page}
-              </div>
-            ),
-        )}
+      {adaptivePaginationPages(amountOfPages(), +pageParam).map(
+        (page, index) =>
+          page !== '...' ? (
+            <button
+              className={classNames('pagination__button', {
+                pagination__active: +pageParam === page,
+              })}
+              key={page}
+              onClick={() => handlePageChange(+page)}
+            >
+              {page}
+            </button>
+          ) : (
+            <div className="pagination__darks" key={`${page}${index}`}>
+              {page}
+            </div>
+          ),
+      )}
 
-        <button
-          disabled={+pageParam === amountOfPages()[amountOfPages().length - 1]}
-          className={classNames('pagination__button pagination__button--next', {
-            'pagination__button--next--disabled':
-              +pageParam === amountOfPages()[amountOfPages().length - 1],
-          })}
-          onClick={handleNextPageChange}
-        />
-      </div>
-    )
-  );
+      <button
+        disabled={+pageParam === amountOfPages()[amountOfPages().length - 1]}
+        className={classNames('pagination__button pagination__button--next', {
+          'pagination__button--next--disabled':
+            +pageParam === amountOfPages()[amountOfPages().length - 1],
+        })}
+        onClick={handleNextPageChange}
+      />
+    </div>
+  ) : null;
 };
